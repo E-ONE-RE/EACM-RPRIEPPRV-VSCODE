3fba2bb169a6dcfae1a499d651c5a41b80878d3c
Giorgio Cremonesi <gcremonesi@eonegroup.it>
2026-09-22 17:49:19 +0200

    Aggiornamento codice

--- eacm.rpriepprv/webapp/ext/controller/PrintPdfHelper.js
+++ eacm.rpriepprv/webapp/ext/controller/PrintPdfHelper.js
@@ -7,7 +7,7 @@ sap.ui.define([
     "sap/ui/model/FilterOperator",
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/library",
-    "eacm/rprpriepprv/ext/controller/MessageLogHelper"
+    "eacm/rpriepprv/ext/controller/MessageLogHelper"
 // eslint-disable-next-line max-params
 ], function (Button, CheckBox, Dialog, VBox, Filter, FilterOperator, JSONModel, coreLibrary, MessageLogHelper) {
     "use strict";
@@ -90,7 +90,7 @@ sap.ui.define([
     function _buildOptionsModel() {
         return new JSONModel({
             DetailPrint: false,
-            PrintWithDueDate: false,
+            PrintWithDueDate: false
         });
     }
 
@@ -252,13 +252,14 @@ sap.ui.define([
     }
 
     async function _downloadAttachmentStream(oContext, oResult) {
+        var sLanguage = sap.ui.getCore().getConfiguration().getLanguage().split("-")[0];
         var sAttachmentUrl = _buildAttachmentUrl(oContext, oResult);
         var aCandidateUrls;
         var oLastError;
         var oBlob;
 
         if (!sAttachmentUrl) {
-            var sLanguage = sap.ui.getCore().getConfiguration().getLanguage().split("-")[0];
+            sLanguage = sap.ui.getCore().getConfiguration().getLanguage().split("-")[0];
             if (sLanguage === "it") {
                throw new Error("Il servizio non ha restituito il link dell''allegato PDF"); //i18n>errorPdfLink
             } else {
@@ -278,7 +279,7 @@ sap.ui.define([
         }
 
         if (!oBlob) {
-            var sLanguage = sap.ui.getCore().getConfiguration().getLanguage().split("-")[0];
+            sLanguage = sap.ui.getCore().getConfiguration().getLanguage().split("-")[0];
             if (sLanguage === "it") {
                throw oLastError || new Error("Download PDF fallito"); //i18n>errorPdfDownload
             } else {
